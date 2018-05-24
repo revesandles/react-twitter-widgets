c8f9b9644043446de56fe79a3ab880c76e2162b8
Steve Randles <revesandles@gmail.com>
2018-05-24 10:40:34 -0400

    adding options

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,23 +7,25 @@ import AbstractWidget from './AbstractWidget'
 export default class Grid extends React.Component {
   static propTypes = {
     collectionId: PropTypes.string.isRequired,
+    options: PropTypes.object,
     onLoad: PropTypes.func,
   };
 
   static defaultProps = {
+    options: {},
     onLoad: () => {},
   };
 
   shouldComponentUpdate(nextProps) {
     const changed = (name) => !isEqual(this.props[name], nextProps[name])
-    return changed('collectionId')
+    return changed('collectionId') || changed('options')
   }
 
   ready = (tw, element, done) => {
-    const { collectionId, onLoad } = this.props
+    const { collectionId, options, onLoad } = this.props
 
-    // Just need the collectionId, you can create a collection with tweetdeck (https://help.twitter.com/en/using-twitter/advanced-tweetdeck-features)
-    tw.widgets.createGridFromCollection(collectionId, element)
+    // Just need the collectionId and optional option. You can create a collection with tweetdeck (https://help.twitter.com/en/using-twitter/advanced-tweetdeck-features)
+    tw.widgets.createGridFromCollection(collectionId, element, cloneDeep(options))
     .then(() => {
       // Widget is loaded
       done()
